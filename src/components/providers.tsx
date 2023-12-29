8ae958dd2491cee2e898de225585bd138ef8560f
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-12-29 12:47:07 -0300

    🔥 chore: add shadcn/ui

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,11 +1,15 @@
 import { router } from "@/routes/router"
 import { Toaster } from "react-hot-toast"
 import { RouterProvider } from "react-router-dom"
+import { TooltipProvider } from "./ui/tooltip"
 
 export default function Providers() {
   return (
     <>
-      <RouterProvider router={router} />
+      <TooltipProvider delayDuration={300}>
+        <RouterProvider router={router} />
+      </TooltipProvider>
+
       <Toaster />
     </>
   )
