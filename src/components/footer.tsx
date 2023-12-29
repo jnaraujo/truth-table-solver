04f17b6201fd0a2086c27fed420672e8d6bba667
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-12-29 20:27:00 -0300

    ♻️ refactor: footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,26 @@
+export default function Footer() {
+  return (
+    <footer className="mt-auto flex h-16 items-center justify-center">
+      <p className="text-center text-sm">
+        <a
+          href="https://github.com/jnaraujo/truth-table-solver"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500"
+        >
+          Código fonte do projeto.
+        </a>
+        <br />O código do algoritmo de Petrick foi baseado no código de{" "}
+        <a
+          href="https://github.com/charlie-coleman/main-website/blob/master/experiments/kmap/ts/petrick.ts"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500"
+        >
+          Charlie Coleman
+        </a>
+        .
+      </p>
+    </footer>
+  )
+}
--- src/routes/page.tsx
+++ src/routes/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer"
 import Header from "@/components/header"
 import Table from "@/components/table"
 
@@ -11,28 +12,7 @@ export function Component() {
         </main>
       </div>
 
-      <footer className="mt-auto flex h-16 items-center justify-center">
-        <p className="text-center">
-          <a
-            href="https://github.com/jnaraujo/truth-table-solver"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500"
-          >
-            Código fonte do projeto.
-          </a>
-          <br />O código do algoritmo de Petrick foi baseado no código de{" "}
-          <a
-            href="https://github.com/charlie-coleman/main-website/blob/master/experiments/kmap/ts/petrick.ts"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500"
-          >
-            Charlie Coleman
-          </a>
-          .
-        </p>
-      </footer>
+      <Footer />
     </>
   )
 }
