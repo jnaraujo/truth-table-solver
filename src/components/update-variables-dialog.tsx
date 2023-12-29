8d351ec8186c8869cec4db6d8a2b7d52cad6555a
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-12-29 13:40:06 -0300

    🐛 fix: duplicated button on update variables dialog

--- src/components/update-variables-dialog.tsx
+++ src/components/update-variables-dialog.tsx
@@ -57,7 +57,7 @@ export default function UpdateVariablesDialog({ children }: Props) {
         </div>
 
         <DialogFooter>
-          <DialogClose disabled={!hasChanged}>
+          <DialogClose asChild>
             <Button
               type="submit"
               onClick={handleUpdateVariables}
