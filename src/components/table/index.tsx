0ca688022302f6108f95b22f0b69b414e317aa7f
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-01-01 20:18:59 -0300

    ✨ feat: add scroll to table

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -3,6 +3,7 @@ import Line from "./line"
 import { useMemo } from "react"
 import Header from "./header"
 import Footer from "./footer"
+import { ScrollArea, ScrollBar } from "../ui/scroll-area"
 
 export default function Table() {
   const tableLength = useTruthTableStore((s) => s.table.length)
@@ -16,15 +17,19 @@ export default function Table() {
   return (
     <article className="flex w-full flex-col gap-4">
       <div className="max-w-[90vw] overflow-x-auto md:w-full">
-        <div className="flex min-w-min flex-col gap-2 p-2">
-          <Header />
+        <ScrollArea className="h-[50vh] p-3 md:h-[60vh]">
+          <div className="flex w-max flex-col gap-2">
+            <Header />
 
-          <div className="flex min-h-40 flex-col items-center gap-2">
-            {indexTable.map((index) => (
-              <Line key={index} lineIndex={index} />
-            ))}
+            <div className="flex flex-col items-center gap-2">
+              {indexTable.map((index) => (
+                <Line key={index} lineIndex={index} />
+              ))}
+            </div>
           </div>
-        </div>
+
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
       </div>
 
       <Footer />
