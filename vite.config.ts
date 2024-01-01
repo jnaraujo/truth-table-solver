8755dc2e8b2701827df63243d4e3f1c786ce109d
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-01-01 19:37:05 -0300

    🔥 chore: add splitVendorChunkPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import path from "path"
-import { defineConfig } from "vite"
+import { defineConfig, splitVendorChunkPlugin } from "vite"
 import react from "@vitejs/plugin-react"
 import webfontDownload from "vite-plugin-webfont-dl"
 import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), webfontDownload(), svgr()],
+  plugins: [react(), webfontDownload(), svgr(), splitVendorChunkPlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
