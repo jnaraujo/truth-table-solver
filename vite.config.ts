b38b7837ae4224bbfb284bafbdbe10cb520dd9ec
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-01-02 16:57:40 -0300

    🔥 chore: change font load

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import path from "path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import webfontDownload from "vite-plugin-webfont-dl"
+import Unfonts from "unplugin-fonts/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), webfontDownload()],
+  plugins: [
+    react(),
+    Unfonts({
+      fontsource: {
+        families: [
+          {
+            name: "Inter Variable",
+            weights: [400, 500],
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
