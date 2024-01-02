72415b4693cdbcb172b5faa9cbca7e7a3dc58438
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-01-01 22:24:18 -0300

    ✨ feat: add sticky top on table header

--- src/components/table/header.tsx
+++ src/components/table/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   const variables = useTruthTableStore((s) => s.variables)
 
   return (
-    <header className="flex justify-center gap-4 border-b border-zinc-300 px-1 py-1">
+    <header className="sticky top-0 flex justify-center gap-4 border-b border-zinc-300 bg-zinc-50 px-1 py-1 shadow-sm">
       {variables.map((item, index) => (
         <Cell key={item + index}>
           <p className="text-sm font-medium text-zinc-500">{item}</p>
